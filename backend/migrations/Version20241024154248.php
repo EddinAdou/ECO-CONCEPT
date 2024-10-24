ff3f692c8b6aa86be5958cdcb2ffba11d1aef152
EddinAdou <k.adou@it-students.fr>
2024-10-24 19:24:34 +0200

    fonctionnment cors

---
+++ backend/migrations/Version20241024154248.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241024154248 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE analyze_site (id INT AUTO_INCREMENT NOT NULL, url VARCHAR(255) NOT NULL, poids_total DOUBLE PRECISION NOT NULL, nb_requetes INT NOT NULL, empreinte_carbone DOUBLE PRECISION NOT NULL, empreinte_eau DOUBLE PRECISION NOT NULL, date_analyse DATETIME NOT NULL, score DOUBLE PRECISION NOT NULL, optimiser_images TINYINT(1) NOT NULL, reduire_requettes TINYINT(1) NOT NULL, note VARCHAR(1) NOT NULL, appreciation VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE analyze_site');
+        $this->addSql('DROP TABLE user');
+    }
+}
